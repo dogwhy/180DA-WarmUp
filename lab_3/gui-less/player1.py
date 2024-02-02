e59ac379516371c7f9821e2afbaa870e717f7757
dogwhy <shafeekhan@gmail.com>
2024-02-01 20:45:13 -0800

    working gui, but kinda lame lol

--- lab_3/gui-less/player1.py
+++ lab_3/gui-less/player1.py
@@ -1,13 +1,9 @@
 import paho.mqtt.client as mqtt
-from rps_gui import RPSGUI
 
 result_received = False  # Flag to indicate when the result is received
 
 def on_connect(client, userdata, flags, rc):
-    print("Connection returned result: " + str(rc))
     client.subscribe("rps/player1/result", qos=1)
-    # Additional subscription for the other player's move
-    client.subscribe("rps/player2/result", qos=1)
 
 def on_disconnect(client, userdata, rc):
     if rc != 0:
@@ -19,11 +15,11 @@ def on_message(client, userdata, message):
     global result_received
     result = str(message.payload, 'utf-8')
     if result == 'win':
-        gui.result_label.config(text="You won!")
+        print("You won!")
     elif result == 'lose':
-        gui.result_label.config(text="You lose!")
+        print("You lose!")
     elif result == 'draw':
-        gui.result_label.config(text="It's a draw!")
+        print("It's a draw!")
     result_received = True  # Set the flag to indicate that the result is received
 
 client = mqtt.Client()
@@ -34,13 +30,18 @@ client.on_message = on_message
 client.connect_async('mqtt.eclipseprojects.io')
 client.loop_start()
 
-player_name = "player2"
+player_name = "player1"
 topic = f"rps/{player_name}"
 
-gui = RPSGUI(player_name, client, topic)
 while True:
-    gui.start()
+    move = input("Enter your move (r for rock, p for paper, s for scissors): ")
+    if move == "q":
+        break
+    client.publish(topic, move, qos=1)
+    
     # Wait for the other player to make a move
     result_received = False  # Reset the flag
     while not result_received:
-        client.loop()  # Process incoming messages
+        pass  # Wait until the result is received
+
+print("Round completed. Make another move or 'q' to quit.")
--- lab_3/player1.py
+++ lab_3/player1.py
@@ -1,9 +1,13 @@
 import paho.mqtt.client as mqtt
+from rps_gui import RPSGUI
 
 result_received = False  # Flag to indicate when the result is received
 
 def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: " + str(rc))
     client.subscribe("rps/player1/result", qos=1)
+    # Additional subscription for the other player's move
+    client.subscribe("rps/player2/result", qos=1)
 
 def on_disconnect(client, userdata, rc):
     if rc != 0:
@@ -15,11 +19,11 @@ def on_message(client, userdata, message):
     global result_received
     result = str(message.payload, 'utf-8')
     if result == 'win':
-        print("You won!")
+        gui.result_label.config(text="You won!")
     elif result == 'lose':
-        print("You lose!")
+        gui.result_label.config(text="You lose!")
     elif result == 'draw':
-        print("It's a draw!")
+        gui.result_label.config(text="It's a draw!")
     result_received = True  # Set the flag to indicate that the result is received
 
 client = mqtt.Client()
@@ -33,15 +37,10 @@ client.loop_start()
 player_name = "player1"
 topic = f"rps/{player_name}"
 
+gui = RPSGUI(player_name, client, topic)
 while True:
-    move = input("Enter your move (r for rock, p for paper, s for scissors): ")
-    if move == "q":
-        break
-    client.publish(topic, move, qos=1)
-    
+    gui.start()
     # Wait for the other player to make a move
     result_received = False  # Reset the flag
     while not result_received:
-        pass  # Wait until the result is received
-
-print("Round completed. Make another move or 'q' to quit.")
+        client.loop()  # Process incoming messages
